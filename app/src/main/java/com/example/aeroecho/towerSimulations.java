d348910beeb1572fc9506d9a0d710cb582675f1a
Rhyz-Jermaine <capstoneproject.ait224@gmail.com>
2024-06-16 00:09:49 +0800

    tower

--- app/src/main/java/com/example/aeroecho/towerSimulations.java
+++ app/src/main/java/com/example/aeroecho/towerSimulations.java
@@ -1,6 +1,8 @@
 package com.example.aeroecho;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +23,10 @@ public class towerSimulations extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void onOfflineButtonClick(View view) {
+        Intent intent = new Intent(this, Simulation_generated.class);
+        startActivity(intent);
+    }
+
 }
