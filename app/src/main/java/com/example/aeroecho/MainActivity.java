8eaf6ad3d239824b348574218e61652e118a38c6
Rhyz-Jermaine <capstoneproject.ait224@gmail.com>
2024-08-29 15:39:47 +0800

    update

--- app/src/main/java/com/example/aeroecho/MainActivity.java
+++ app/src/main/java/com/example/aeroecho/MainActivity.java
@@ -37,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void onCreateButtonClick(View view) {
+
+        Intent intent = new Intent(this, createstudentacc.class);
+
+        startActivity(intent);
+    }
+
     public void onAdminButtonClick(View view) {
 
         Intent intent = new Intent(this, loginadmin.class);
